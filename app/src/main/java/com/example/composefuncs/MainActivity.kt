9906cd0e4a56741deb7f2c558ad652e107975649
zoirbekmuxtorov <zoirbekmuxtorov98@gmai.com>
2023-07-24 17:42:04 +0500

    Textlarni 2 xil stilga solish

--- app/src/main/java/com/example/composefuncs/MainActivity.kt
+++ app/src/main/java/com/example/composefuncs/MainActivity.kt
@@ -29,10 +29,12 @@ import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.BaselineShift
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composefuncs.ui.theme.ComposeFuncsTheme
@@ -46,24 +48,40 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    CustomText()
+                    SuperScriptText(normalText = "Hello", superText = "World!")
                 }
             }
         }
     }
 }
 @Composable
-fun CustomText() {
-    SelectionContainer {
-        Column {
-            Text(text = "Hello World!")
-            DisableSelection {
-                Text(text = "Hello World!")
-            }
-            Text(text = "Hello World!")
+fun SuperScriptText(
+    normalText:String,
+    normalFontSize: TextUnit = MaterialTheme.typography.titleLarge.fontSize,
+    superText:String,
+    superFontSize:TextUnit = MaterialTheme.typography.labelMedium.fontSize,
+    superTextFontWeight:FontWeight = FontWeight.Normal
+) {
+Text(
+    buildAnnotatedString {
+        withStyle(
+            style = SpanStyle(
+                fontSize = normalFontSize
+            )
+        ){
+            append(normalText)
         }
-
+        withStyle(
+            style = SpanStyle(
+                fontSize = superFontSize,
+                fontWeight = superTextFontWeight,
+                baselineShift = BaselineShift.Superscript
+            )
+        ){
+            append(superText)
+        }
     }
+)
 
 
 }
@@ -73,7 +91,7 @@ fun CustomText() {
 fun DefaultPreview() {
     ComposeFuncsTheme {
         Column(modifier = Modifier.fillMaxSize()) {
-            CustomText()
+            SuperScriptText(normalText = "Hello", superText = "World!")
         }
 
     }
