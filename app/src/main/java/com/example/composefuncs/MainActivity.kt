36c0c7dfa13cb803adb9f1b87b5949dbee0bec85
zoirbekmuxtorov <zoirbekmuxtorov98@gmai.com>
2023-07-25 11:20:08 +0500

    input shaklini yasash

--- app/src/main/java/com/example/composefuncs/MainActivity.kt
+++ app/src/main/java/com/example/composefuncs/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.composefuncs
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -14,14 +15,27 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.selection.DisableSelection
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -31,6 +45,8 @@ import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.BaselineShift
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
@@ -39,6 +55,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.wear.compose.material3.IconButton
 import com.example.composefuncs.ui.theme.ComposeFuncsTheme
 
 class MainActivity : ComponentActivity() {
@@ -48,31 +65,68 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeFuncsTheme {
                 // A surface container using the 'background' color from the theme
-                Column(
-                    modifier = Modifier.fillMaxWidth().background(MaterialTheme.colorScheme.onBackground).padding(20.dp)
+                Surface(
+                    color = MaterialTheme.colorScheme.background
 
                 ) {
-                    ExpandableCardPreview()
+                    Greeting()
                 }
             }
         }
     }
 }
 
+@ExperimentalMaterial3Api
+@Composable
+fun Greeting() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        var text by remember { mutableStateOf("Type here...") }
 
+//        BasicTextField -> bunda hechnarsa bolyamdi faqat text ozini yozsa bo'ladi
+//        OutlinedTextField -> bunda borderga chiziq chiqarib beradi
+
+        TextField(value = text, onValueChange = { newText ->
+            text = newText
+        },
+            label = {
+                Text(text = "Title")
+            },
+            leadingIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Filled.Email, contentDescription = "Email")
+                    
+                }
+            },
+            trailingIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Filled.Check, contentDescription = "Email")
+
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    Log.d("imeAction", "Greeting: $this")
+                }
+            )
+        )
+    }
+
+}
 
 @ExperimentalMaterial3Api
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeFuncsTheme {
-        Column(
-            modifier = Modifier.fillMaxWidth().background(MaterialTheme.colorScheme.onBackground).padding(20.dp)
-        ) {
-            ExpandableCardPreview()
-        }
-
-
+        Greeting()
     }
 }
 
