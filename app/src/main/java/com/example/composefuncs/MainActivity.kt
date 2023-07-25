c5c3dff85920f5d71a41c515fcf7459b1a041b04
zoirbekmuxtorov <zoirbekmuxtorov98@gmai.com>
2023-07-25 13:21:56 +0500

    gradient button

--- app/src/main/java/com/example/composefuncs/MainActivity.kt
+++ app/src/main/java/com/example/composefuncs/MainActivity.kt
@@ -1,62 +1,31 @@
 package com.example.composefuncs
 
+import androidx.compose.ui.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.text.selection.DisableSelection
-import androidx.compose.foundation.text.selection.SelectionContainer
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.ParagraphStyle
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.BaselineShift
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.wear.compose.material3.IconButton
+import androidx.wear.compose.material3.Text
 import com.example.composefuncs.ui.theme.ComposeFuncsTheme
+import com.example.composefuncs.ui.theme.Pink40
+import com.example.composefuncs.ui.theme.Purple40
+import com.example.composefuncs.ui.theme.PurpleGrey40
 
 class MainActivity : ComponentActivity() {
     @ExperimentalMaterial3Api
@@ -64,69 +33,58 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeFuncsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    color = MaterialTheme.colorScheme.background
-
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Greeting()
+                    GradientButtonPreview()
                 }
             }
         }
     }
 }
 
-@ExperimentalMaterial3Api
 @Composable
-fun Greeting() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
+fun GradientButton(
+    text: String,
+    textColor: Color,
+    gradient: Brush,
+    onClick: () -> Unit
+) {
+    Button(
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent
+        ),
+        contentPadding = PaddingValues(),
+        onClick = { onClick() },
     ) {
-        var text by remember { mutableStateOf("Type here...") }
+        Box(
+            modifier = Modifier
+                .background(gradient)
+                .padding(horizontal = 16.dp, vertical = 8.dp),
+            contentAlignment = Alignment.Center,
+        ) {
+            Text(text = text, color = textColor)
+        }
+    }
 
-//        BasicTextField -> bunda hechnarsa bolyamdi faqat text ozini yozsa bo'ladi
-//        OutlinedTextField -> bunda borderga chiziq chiqarib beradi
+}
 
-        TextField(value = text, onValueChange = { newText ->
-            text = newText
-        },
-            label = {
-                Text(text = "Title")
-            },
-            leadingIcon = {
-                IconButton(onClick = { /*TODO*/ }) {
-                    Icon(imageVector = Icons.Filled.Email, contentDescription = "Email")
-                    
-                }
-            },
-            trailingIcon = {
-                IconButton(onClick = { /*TODO*/ }) {
-                    Icon(imageVector = Icons.Filled.Check, contentDescription = "Email")
-
-                }
-            },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Email,
-                imeAction = ImeAction.Search
-            ),
-            keyboardActions = KeyboardActions(
-                onSearch = {
-                    Log.d("imeAction", "Greeting: $this")
-                }
+@Preview
+@Composable
+fun GradientButtonPreview() {
+    GradientButton(
+        text = "Button", textColor = Color.White, gradient = Brush.horizontalGradient(
+            colors = listOf(
+                Purple40,
+                PurpleGrey40,
+                Pink40
             )
         )
-    }
+    ) {
 
-}
-
-@ExperimentalMaterial3Api
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeFuncsTheme {
-        Greeting()
     }
 }
+
 
