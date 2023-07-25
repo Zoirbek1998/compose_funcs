418642670af536095a1db8eafe4f170e588fa8c3
zoirbekmuxtorov <zoirbekmuxtorov98@gmai.com>
2023-07-25 14:05:42 +0500

    LazyColumn - Forget about RecyclerView Adapter - Jetpack Compose

--- app/src/main/java/com/example/composefuncs/MainActivity.kt
+++ app/src/main/java/com/example/composefuncs/MainActivity.kt
@@ -1,90 +1,82 @@
 package com.example.composefuncs
 
-import androidx.compose.ui.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.text.TextRange
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material3.Text
+
 import com.example.composefuncs.ui.theme.ComposeFuncsTheme
-import com.example.composefuncs.ui.theme.Pink40
-import com.example.composefuncs.ui.theme.Purple40
-import com.example.composefuncs.ui.theme.PurpleGrey40
+
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalFoundationApi
     @ExperimentalMaterial3Api
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeFuncsTheme {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
+
+                val sections = listOf("A", "B", "C", "D", "E", "F", "G")
+
+                LazyColumn(
+                    contentPadding = PaddingValues(all = 12.dp),
+                    verticalArrangement = Arrangement.spacedBy(12.dp)
                 ) {
-                    GradientButtonPreview()
+                    sections.forEach { section ->
+                        stickyHeader {
+                            Text(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(Color.LightGray)
+                                    .padding(12.dp),
+                                text = "Section $section"
+                            )
+                        }
+
+                        items(10){
+                            Text(
+                                modifier = Modifier.padding(12.dp)
+                                ,text = "Item $it from the section $section",
+                                color = Color.Blue
+                            )
+                        }
+
+                    }
                 }
+
+
+//                val personRepository = PersonRepository()
+//                val getAllData = personRepository.getAllData()
+//
+//                LazyColumn(
+//                    contentPadding = PaddingValues(all = 12.dp),
+//                    verticalArrangement = Arrangement.spacedBy(12.dp)
+//                ){
+//                    itemsIndexed(items = getAllData) {index, person ->
+//                        Log.d("LazyColumn", "onCreate:  ")
+//                        CustomItem(person = person)
+//                    }
+//                }
             }
         }
     }
 }
 
-@Composable
-fun GradientButton(
-    text: String,
-    textColor: Color,
-    gradient: Brush,
-    onClick: () -> Unit
-) {
-    Button(
-        colors = ButtonDefaults.buttonColors(
-            containerColor = Color.Transparent
-        ),
-        contentPadding = PaddingValues(),
-        onClick = { onClick() },
-    ) {
-        Box(
-            modifier = Modifier
-                .background(gradient)
-                .padding(horizontal = 16.dp, vertical = 8.dp),
-            contentAlignment = Alignment.Center,
-        ) {
-            Text(text = text, color = textColor)
-        }
-    }
-
-}
-
-@Preview
-@Composable
-fun GradientButtonPreview() {
-    GradientButton(
-        text = "Button", textColor = Color.White, gradient = Brush.horizontalGradient(
-            colors = listOf(
-                Purple40,
-                PurpleGrey40,
-                Pink40
-            )
-        )
-    ) {
-
-    }
-}
 
 
